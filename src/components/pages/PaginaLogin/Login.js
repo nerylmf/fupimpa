09cc47e12ead3dac1d5907c88450e33812257738
kellyfreitas <kellyfreitas418@gmail.com>
2021-08-10 19:18:10 -0300

    Página login

---
+++ src/components/pages/PaginaLogin/Login.js
@@ -0,0 +1,33 @@
+import { NavLink } from 'react-router-dom'
+import '../../common/colors/colors.css'
+import './Login.css'
+
+export function Login() {
+    return (
+        
+        <div className="background-login back-verde">
+            <div className="card-login back-preto">
+                <form className="form">
+                    
+                    <label className="txt-login branco" for="usuario">Usuário:</label>
+                    <input  className="input-usuario back-branco" type="text" name="campo-usuario" ></input>
+                    <label className="txt-login branco" for="passoword">Senha:</label>
+                    <input className="input-senha back-branco" type="password" name="password"></input>
+                    <div className="sub-opcoes"> 
+                        <input className="check " type="checkbox" ></input>
+                        <p className="lembrar branco" >Lembrar de mim </p>
+                        <a className="esqueceu-senha branco">Esqueceu a senha?</a>
+                    </div>
+                    <NavLink
+                            exact
+                            className="botao-entrar back-laranja preto"
+                            to="/">
+                            Entrar
+                    </NavLink>
+                   
+                </form>
+
+            </div>
+        </div>
+    )
+}
