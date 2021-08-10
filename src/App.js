45137a20696683b2bd907cfce4386e25d27f000a
kellyfreitas <kellyfreitas418@gmail.com>
2021-08-10 19:22:31 -0300

    add rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { BrowserRouter, Route, Router } from 'react-router-dom';
 import './App.css';
+import { CodeEditor } from './components/common/Editor/Editor';
 import { Home } from './components/pages/Home/Home';
+import { PaginaAtividadeX } from './components/pages/PaginaAtividade/PaginaAtividade';
+import { Login } from './components/pages/PaginaLogin/Login';
+import history from './history';
 
 function App() {
   return (
-    <Home></Home>
+ 
+    <Router history={history}>
+        <Route path="/login">
+            <Login></Login>
+        </Route>
+       
+        <Route exact path="/">
+            <Home></Home>
+        </Route>
+
+        <Route path="/Atividade"
+           component={PaginaAtividadeX}>
+        </Route>
+
+        
+    </Router>
+    
   );
 }
 
 export default App;
+
