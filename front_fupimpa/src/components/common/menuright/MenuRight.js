3559ed186d83758e12c33ceb360f088978a95240
nerylmf <nerylf@alu.ufc.br>
2021-08-17 13:30:31 -0300

    Implementação da correção
    get de matrizes de pixel implementado, falta logica de comparacao

--- front_fupimpa/src/components/common/menuright/MenuRight.js
+++ front_fupimpa/src/components/common/menuright/MenuRight.js
@@ -1,23 +1,83 @@
+import { useState } from 'react';
+import { Expectativa } from '../../p5/Expectativa';
+import { Realidade } from '../../p5/Realidade';
 import './MenuRight.css';
 
-export function CardResultadoAtv(props) {
+export function CardExpectativa(props) {
     return (
 
         <div className="card-resultado back-branco">
             <div className="card-text ">
                 <span className="resul-title preto">{props.title}</span>
             </div>
+            <Expectativa
+                expectativa={props.expectativa}
+                setExpectativa={props.setExpectativa}
+            ></Expectativa>
         </div>
     )
 }
+
+export function CardRealidade(props) {
+
+    return (
+        <div className="card-resultado back-branco">
+            <div className="card-text ">
+                <span className="resul-title preto">{props.title}</span>                
+            </div>
+            <Realidade
+                realidade={props.realidade}
+                setRealidade={props.setRealidade}
+            ></Realidade>
+        </div>
+    )
+}
+
+
 export function MenuRight() {
+    const [realidade, setRealidade] = useState(0);
+    const [expectativa, setExpectativa] = useState(0);
+    //console.log(realidade);
+
     return (
         <div className="background-exp-real back-verde">
-            <CardResultadoAtv title="Expectativa"></CardResultadoAtv>
-            <CardResultadoAtv title="Realidade" ></CardResultadoAtv>
+
+            <CardExpectativa 
+                title="Expectativa"                
+                expectativa={expectativa}
+                setExpectativa={setExpectativa}
+            ></CardExpectativa>
+
+            <CardRealidade 
+                title="Realidade" 
+                realidade={realidade}
+                setRealidade={setRealidade} 
+            ></CardRealidade>
+
+            <Comparar 
+                realidade={realidade}
+                setRealidade={setRealidade}   
+                
+                expectativa={expectativa}
+                setExpectativa={setExpectativa}
+            ></Comparar>
 
         </div>
     );
 }
 
 
+export function Comparar(props) {
+    function testar(){
+        //props.setExpectativa(props.expectativa + 1);
+        //props.setRealidade(props.realidade + 2)
+        console.log(props.realidade); 
+        console.log(props.expectativa);            
+    }
+
+    return(
+        <button onClick={testar} className="botao-comparar">
+            Comparar
+        </button>
+    );
+}
