e6043884b63fec1d29979f407553439727313de6
kellyfreitas <kellyfreitas418@gmail.com>
2021-08-27 19:28:07 -0300

    ajustes Atividade

--- front_fupimpa/src/components/common/atividades/Atividades.js
+++ front_fupimpa/src/components/common/atividades/Atividades.js
@@ -3,11 +3,17 @@ import '../colors/colors.css';
 import './atividades.css'
 
 export function Atividades(){
+   
+
     return(
-        <div className="container-atividades preto back-branco">
+        <div  className="container-atividades preto back-branco">
             <CardAtividade title="Galinha no tapa" desc="Dona Lourdes é cozinheira do Restaurante..."></CardAtividade>
             <CardAtividade title="Abduzido" desc="Abdu é um alienigena que esta de passeio..."></CardAtividade>
             <CardAtividade title="A Fuga" desc="Fupimpinha entrou em desespero e começou a correr.."></CardAtividade>
+            <CardAtividade title="OPA" desc="asd fasd asdfa sdfasdfas dba ajahb dvjh"></CardAtividade>
+            <CardAtividade title="OPA" desc="asd fasd asdfa sdfasdfas dba ajahb dvjh"></CardAtividade>
+            <CardAtividade title="OPA" desc="asd fasd asdfa sdfasdfas dba ajahb dvjh"></CardAtividade>
+            <CardAtividade title="OPA" desc="asd fasd asdfa sdfasdfas dba ajahb dvjh"></CardAtividade>
             <CardAtividade title="OPA" desc="asd fasd asdfa sdfasdfas dba ajahb dvjh"></CardAtividade>
             <CardAtividade title="OPA" desc="asd fasd asdfa sdfasdfas dba ajahb dvjh"></CardAtividade>
             <CardAtividade title="OPA" desc="asd fasd asdfa sdfasdfas dba ajahb dvjh"></CardAtividade>
@@ -27,7 +33,7 @@ export function CardAtividade(props){
     return(
         <div className="card-atividade" onClick={foiClicado}>
             {/* apenas uma imagem de teste */}
-            <div className="img-atv back-preto"></div>
+            <div className="img-atv back-laranja"></div>
             <div className="card-text">
                 <span className="card-title preto">{props.title}</span>
                 <p className="card-desc preto">{props.desc}</p>
