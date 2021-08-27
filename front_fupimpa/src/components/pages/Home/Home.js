9bb872b48b0c7b9a27f6895a96d5d7080f9b8123
kellyfreitas <kellyfreitas418@gmail.com>
2021-08-27 19:25:24 -0300

    ajustes home

--- front_fupimpa/src/components/pages/Home/Home.js
+++ front_fupimpa/src/components/pages/Home/Home.js
@@ -3,17 +3,30 @@ import '../../common/colors/colors.css';
 import { MenuLeft } from '../../common/menuleft/MenuLeft';
 import { NavBar } from '../../common/navbar/NavBar';
 import './Home.css';
+import filtro from './filtro.svg';
 
-export function Home(){
-    return(
+
+export function Home() {
+    return (
         <div className="background-home branco back-preto">
-            
+
             <NavBar></NavBar>
             <div className="container-home">
                 <MenuLeft></MenuLeft>
                 <Atividades></Atividades>
             </div>
-            
+            <div className='container-filtro'>
+                <div class="botao-filtrar">
+                    <div class="container-dropdown back-preto">
+                        <a className="link-filtro branco" href="">Concluidas</a>
+                        <a className="link-filtro branco" href="">Corrigidas</a>
+                        <a className="link-filtro branco" href="">Iniciadas</a>
+                    </div>
+                    <img src={filtro} className="filtro-dropdown" alt="Botao filtrar"  ></img>
+
+
+                </div>
+            </div>
         </div>
     );
 }
