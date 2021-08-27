f86a4cd8c1925b558de76bc197f778ad7aa07680
kellyfreitas <kellyfreitas418@gmail.com>
2021-08-27 19:24:54 -0300

    ajustes pagina cadastro

--- front_fupimpa/src/components/pages/PaginaCadastro/PaginaCadastro.js
+++ front_fupimpa/src/components/pages/PaginaCadastro/PaginaCadastro.js
@@ -1,38 +1,40 @@
 import { NavLink } from 'react-router-dom'
 import '../../common/colors/colors.css'
 //import '../PaginaLogin/Login.css'
+import '../PaginaLogin/Login.css'
 import './PaginaCadastro.css'
+
 
 
 export function Cadastro() {
     return (
         
-        <div className="background-login back-verde">
+        <div className="background-cadastro back-verde">
            <span className="txt-bemVindo preto">Bem vindo ao Fupimpa! </span>
            
-            <div className="card-login back-preto">
+            <div className="card back-preto">
                 <form className="form">
                    
-                    <label className="txt-login branco" for="usuario">Usuário:</label>
-                    <input  className="input back-branco" type="text" name="campo-usuario" ></input>
-                    <label className="txt-login branco" for="matricula">Matricula:</label>
-                    <input  className="input back-branco" type="text" name="campo-matricula" ></input>
-                    <label className="txt-login branco" for="passoword">Senha:</label>
-                    <input className="input back-branco" type="password" name="password"></input>
+                    <label className="label-txt branco" for="usuario">Usuário:</label>
+                    <input  className="input-cad back-branco" type="text" name="campo-usuario" ></input>
+                    <label className="label-txt branco" for="matricula">Matricula:</label>
+                    <input  className="input-cad back-branco" type="text" name="campo-matricula" ></input>
+                    <label className="label-txt branco" for="passoword">Senha:</label>
+                    <input className="input-cad back-branco" type="password" name="password"></input>
                    
                     <NavLink
                             exact
-                            className="botao-cadastrar back-laranja preto"
+                            className="botao-confirmar botao-cad back-laranja preto"
                             to="/">
                             Cadastrar
                     </NavLink>
                     
-                    <div className="login branco">
+                    <div className="txt-redirecionar branco">
                         <span>Já é um Fupimpa! 
                             <NavLink
                                 exact
-                                className="login link-login branco"
-                                to="/">
+                                className="txt-redirecionar link-redirecionar branco"
+                                to="/login">
                                  Faça seu login!
                             </NavLink>
                         </span>
