45c895c2dfcff074e1ec04ed6e1c59a083a5fbd1
kellyfreitas <kellyfreitas418@gmail.com>
2021-08-28 16:40:06 -0300

    add NavLink em MenuLeft

--- front_fupimpa/src/components/common/menuleft/MenuLeft.js
+++ front_fupimpa/src/components/common/menuleft/MenuLeft.js
@@ -1,23 +1,166 @@
+
+import { Link, NavLink } from 'react-router-dom';
 import '../colors/colors.css';
 import './menuleft.css';
 
-export function MenuLeft(){
-    return(
+export function MenuLeft() {
+    return (
         <div className="menu-left">
-            <MenuLeftButton colorBack="laranja" label="Algorítmo"></MenuLeftButton>
+            <NavLink
+                exact
+                className=" menu-left-button back-laranja"/*menu-left-button */
+                to="/">
+                    Algorítmo
+            </NavLink>
+            <NavLink
+                exact
+                className="menu-left-button back-vermelho"/*menu-left-button */
+                to="/modulo2">
+                    Expressões
+            </NavLink>
+            <NavLink
+                exact
+                className=" menu-left-button back-verde"/*menu-left-button */
+                to="/modulo3">
+                    Funções
+            </NavLink>
+            <NavLink
+                exact
+                className=" menu-left-button back-branco"/*menu-left-button */
+                to="/modulo4">
+                    Controle de Fluxo
+            </NavLink>
+            {/* <MenuLeftButton colorBack="laranja" label=""></MenuLeftButton>
             <MenuLeftButton colorBack="vermelho" label="Expressões"></MenuLeftButton>
             <MenuLeftButton colorBack="verde" label="Funções"></MenuLeftButton>
-            <MenuLeftButton colorBack="verde" label="Controle de fluxo"></MenuLeftButton>
+            <MenuLeftButton colorBack="verde" label="Controle de fluxo"></MenuLeftButton> */}
 
         </div>
     );
 }
 
-export function MenuLeftButton(props){
+export function MenuLeftButton(props) {
     let classes = "menu-left-button preto back-" + props.colorBack;
-    return(
+    return (
         <div className={classes}>
             {props.label}
         </div>
     );
 }
+
+
+
+
+
+
+
+
+
+
+
+// import { Link, NavLink } from 'react-router-dom';
+// import '../colors/colors.css';
+// import './menuleft.css';
+
+// export function MenuLeft(){
+//     return(
+//         <div className="menu-left">
+//             <MenuLeftButton rota="modulo1"colorBack="laranja" label="Algorítmo"></MenuLeftButton>
+//             <MenuLeftButton colorBack="vermelho" label="Expressões"></MenuLeftButton>
+//             <MenuLeftButton colorBack="verde" label="Funções"></MenuLeftButton>
+//             <MenuLeftButton colorBack="verde" label="Controle de fluxo"></MenuLeftButton>
+
+//         </div>
+//     );
+// }
+
+// // export function MenuLeftButton(props){
+// //     let classes = "menu-left-button preto back-" + props.colorBack;
+// //     return(
+// //         <div className={classes}>
+// //             {props.label}
+// //         </div>
+// //     );
+// // }
+
+// export function MenuLeftButton(props) {
+//     let classes = "menu-left-button  back-" + props.colorBack;
+//     return (
+//         <div className={classes}>
+           
+//             <NavLink
+//                 exact
+//                 className="btn"
+//                 to={"/"+props.rota}>
+//                 {props.label}
+//             </NavLink>
+//         </div>
+//     );
+// }
+
+
+
+
+//v2
+// import { useState } from 'react';
+// import '../colors/colors.css';
+// import './menuleft.css';
+
+// export function MenuLeft(){
+//     return(
+//         <div className="menu-left">
+//             <MenuLeftButton colorBack="laranja" label="Algorítmo"></MenuLeftButton>
+//             <MenuLeftButton colorBack="vermelho" label="Expressões"></MenuLeftButton>
+//             <MenuLeftButton colorBack="verde" label="Funções"></MenuLeftButton>
+//             <MenuLeftButton colorBack="verde" label="Controle de fluxo"></MenuLeftButton>
+
+//         </div>
+//     );
+// }
+
+// export function MenuLeftButton(props){
+//     const [setAtivo, setAtivoState]= useState("");
+
+//     function MostrarModulos(){
+//         setAtivoState(setAtivo==="" ? "ativo-button":"");
+//     }
+    
+
+
+//     let classes = `menu-left-button ${setAtivo} back-` + props.colorBack;
+//     return(
+//         <div className={classes} onClick={MostrarModulos}>
+//             {props.label}
+//         </div>
+//     );
+// }
+
+
+
+
+
+///versão 1
+
+// import '../colors/colors.css';
+// import './menuleft.css';
+
+// export function MenuLeft(){
+//     return(
+//         <div className="menu-left">
+//             <MenuLeftButton colorBack="laranja" label="Algorítmo"></MenuLeftButton>
+//             <MenuLeftButton colorBack="vermelho" label="Expressões"></MenuLeftButton>
+//             <MenuLeftButton colorBack="verde" label="Funções"></MenuLeftButton>
+//             <MenuLeftButton colorBack="verde" label="Controle de fluxo"></MenuLeftButton>
+
+//         </div>
+//     );
+// }
+
+// export function MenuLeftButton(props){
+//     let classes = "menu-left-button preto back-" + props.colorBack;
+//     return(
+//         <div className={classes}>
+//             {props.label}
+//         </div>
+//     );
+// }
